8ea1fb4c4f44792cfe7c53c8a3f95e124cf5afcb
Gino Basiletti <gino@rantmedia.com>
2018-06-27 13:56:33 +0100

    Code tidyup, JUnit test started
    Also changed/regenerated the maps API key because for some reason it
    stopped working after 15 hours of working as expected.

--- app/src/main/java/com/basiletti/gino/citysearcher/MainActivity.java
+++ app/src/main/java/com/basiletti/gino/citysearcher/MainActivity.java
@@ -1,15 +1,11 @@
 package com.basiletti.gino.citysearcher;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.basiletti.gino.citysearcher.enums.FragmentType;
 import com.basiletti.gino.citysearcher.objects.CityObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,19 +24,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadSearchFragment() {
-        SearchFragment searchFragment = (SearchFragment) getSupportFragmentManager().findFragmentByTag("searchFragment");
+        SearchFragment searchFragment = (SearchFragment) getSupportFragmentManager().findFragmentByTag(getString(R.string.search_fragment));
         if (searchFragment == null) {
             searchFragment = new SearchFragment();
         }
-        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, searchFragment, "searchFragment").addToBackStack("searchFragment").commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, searchFragment, getString(R.string.search_fragment)).addToBackStack(getString(R.string.search_fragment)).commit();
     }
 
     public void displayMapFragment(CityObject cityObject) {
-        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentByTag("mapFragment");
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentByTag(getString(R.string.map_fragment));
         if (mapFragment == null) {
             mapFragment = MapFragment.newInstance(cityObject);
         }
-        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, mapFragment, "mapFragment").addToBackStack("mapFragment").commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, mapFragment, getString(R.string.map_fragment)).addToBackStack(getString(R.string.map_fragment)).commit();
     }
 
     private void findViews() {
@@ -50,8 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (getSupportFragmentManager().findFragmentByTag("mapFragment") != null) {
-            getSupportFragmentManager().popBackStack("mapFragment", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        if (getSupportFragmentManager().findFragmentByTag(getString(R.string.map_fragment)) != null) {
+            getSupportFragmentManager().popBackStack(getString(R.string.map_fragment), FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        } else {
+            Intent startMain = new Intent(Intent.ACTION_MAIN);
+            startMain.addCategory(Intent.CATEGORY_HOME);
+            startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(startMain);
+
         }
     }
 }
